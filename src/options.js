224268b45a42c288bd3dd0a2bf9550025e367abd
Akhil Gupta <akhil.gupta.7295@gmail.com>
2025-04-22 22:34:27 +0530

    Added table, with basic info

--- src/options.js
+++ src/options.js
@@ -1,16 +1,41 @@
 "use strict"
 
+const properties = [
+    "id",
+    "windowId",
+    "active",
+    "pinned",
+    "hidden",
+    "discarded",
+    "audible",
+    "autoDiscardable",
+    "autoDiscardable",
+    "isInReaderMode",
+    "successorTabId",
+    "url",
+    "title",
+    "favIconUrl",
+];
 
 window.onload = () => {
+    let header = document.getElementById("header");
+    header.innerHTML += `<th>S. no.</th>`;
+    properties.forEach(x => {
+        header.innerHTML += `<th>${x}</th>`;
+    });
+    
+    // TODO: Need to show live list
     browser.tabs.query({}, tabs => {
+        let tbody = document.getElementById("tbody");
+        let sno = 0;
         tabs.forEach(tab => {
-            document.body.innerText += "\n";
-            document.body.innerText += "\n";
-            document.body.innerText += "\n";
-            document.body.innerText += JSON.stringify(tab, null, 4);
+            let row = document.createElement("tr");
+            row.innerHTML = `<td>${++sno}</td>`;
+            properties.forEach(x => {
+                row.innerHTML += `<td>${tab[x]}</td>`;
+            });
+            tbody.appendChild(row);
         });
-    }).catch((error) => {
-        console.error("Error fetching tabs: ", error);
     });
 
     return;
