8108caa1492725fa3a630fe7bb230ccb8edc03a4
Akhil Gupta <akhil.gupta.7295@gmail.com>
2025-06-15 13:41:18 +0530

    Added more empty listeners

--- src/background.js
+++ src/background.js
@@ -32,3 +32,40 @@ browser.tabs.onCreated.addListener(tabId => UpdateTabCounter(tabId, false));
 browser.tabs.onRemoved.addListener(tabId => UpdateTabCounter(tabId, true));
 browser.browserAction.setBadgeBackgroundColor({ color: "#005f9e" });
 UpdateTabCounter(-1, false);
+
+
+
+
+function UpdateSession(){
+
+}
+
+browser.tabs.onCreated.addListener((_,__) => UpdateSession());
+browser.tabs.onRemoved.addListener((_,__) => UpdateSession());
+browser.tabs.onActivated.addListener(_ => UpdateSession());
+browser.tabs.onAttached.addListener((_,__) => UpdateSession());
+browser.tabs.onDetached.addListener((_,__) => UpdateSession());
+browser.tabs.onMoved.addListener((_,__) => UpdateSession());
+browser.tabs.onReplaced.addListener((_,__) => UpdateSession());
+
+
+function OnUpdate(tabId, changeInfo, tabInfo){
+	
+}
+
+browser.tabs.onUpdated.addListener(
+	OnUpdate,
+	{
+		"properties":[
+			"pinned",
+			"status",
+			"url",
+		]
+	}
+);
+
+function OnSessionChanged(){
+	
+}
+
+browser.sessions.onChanged.addListener(OnSessionChanged);
